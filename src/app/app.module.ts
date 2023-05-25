2dc2b580720d085ccb2b1678dd76dd19dab5ef5a
toleubekbogenbai <toleubek.bogenbai@gmail.com>
2023-05-25 11:59:10 +0600

    Angular Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {CategoriesComponent} from './views/categories/categories.component';
+import { TasksComponent } from './views/tasks/tasks.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        CategoriesComponent,
+        TasksComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/service/data-handler.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {Category} from "../model/Category";
+import {TestData} from "../data/TestData";
+import { Task } from '../model/Task';
+import {BehaviorSubject, Subject} from "rxjs";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataHandlerService {
+
+    tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+    categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
+
+
+    constructor() {
+    }
+
+    // getCategories(): Category[] {
+    //     return TestData.categories;
+    // }
+    fillTasks() {
+        this.tasksSubject.next(TestData.tasks);
+    }
+    fillTasksByCategory(category: Category) {
+        const tasks = TestData.tasks.filter(task => task.category === category);
+        this.tasksSubject.next(tasks);
+    }
+
+}
